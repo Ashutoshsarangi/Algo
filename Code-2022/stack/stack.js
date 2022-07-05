37ec7401441bf479c095d3c40b8ad62d5d104325
Ashutosh Sarangi <ashutoshsarangi95@gmail.com>
2022-07-05 12:05:35 +0300

    Create stack.js

---
+++ Code-2022/stack/stack.js
@@ -0,0 +1,14 @@
+/*
+Stack using an array
+Approach 1:- push and pop
+approach 2:- unshift and  shift
+*/
+
+let stack = [];
+
+stack.push('A');
+stack.push('B');
+stack.push('C');
+console.log(stack);
+stack.pop();
+console.log(stack);
