420b52562c7782a1206f701e0ad62e6acee3f3a7
Ashutosh Sarangi <ashutoshsarangi95@gmail.com>
2022-07-15 11:09:10 +0300

    Create Dijkstra.js

---
+++ Code-2022/Dijkstra Algo/Dijkstra.js
@@ -0,0 +1,9 @@
+// Dijkstra Algo
+
+// Here need to add Quick Priority Queue and Eeach time 
+// we insort it sorts the array based on Priority
+
+
+// then we will add a weighted graph to hold the graph
+// POST that I will add traversal algo (Dijkstra) for finding the sortest 
+// path for Source to destination and alog side with other nodes.
