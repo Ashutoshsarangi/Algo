204c7902207dda8c805a94df9f1981c886871c61
Ashutosh Sarangi <ashutoshsarangi95@gmail.com>
2022-07-25 11:11:35 +0300

    Create DP_fib.js

---
+++ Code-2022/Dynamic Programming/DP_fib.js
@@ -0,0 +1,15 @@
+// Memo the values for easy calculation
+// DP with Memo
+
+const fib = (n, memo = []) => {
+    if(memo[n] !== undefined) return memo[n];
+    if(n <= 2){
+        return 1;
+    }
+
+    const res = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = res;
+    return res;
+} 
+
+fib(5);
